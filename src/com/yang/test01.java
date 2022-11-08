2bb53de9d609f4bac9d6343003786a42ed65d71d
Yang <1565738843@qq.com>
2022-11-08 22:11:23 +0800

    hot-fix commit3

--- src/com/yang/test01.java
+++ src/com/yang/test01.java
@@ -19,6 +19,7 @@ public class test01 {
         System.out.println("吃屎去吧");
         System.out.println("吃屎去吧");
         System.out.println("hot-fix test");
+        System.out.println("hot-fix test2");
 
 
     }
