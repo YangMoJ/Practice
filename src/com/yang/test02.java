1df1f54e225f8efe9824085f80555fa8c241994f
Yang <1565738843@qq.com>
2022-11-08 22:14:29 +0800

    master commit4

---
+++ src/com/yang/test02.java
@@ -0,0 +1,12 @@
+package com.yang;
+
+/**
+ * @author yd
+ * @version 1.0
+ * @date 2022/11/8 22:13
+ */
+public class test02 {
+    public static void main(String[] args) {
+        System.out.println("master");
+    }
+}
